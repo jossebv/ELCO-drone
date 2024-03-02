dac514451f033c1732d5468f9fd38ff939118896
jossebv <josebravopacheco@gmail.com>
2024-03-02 17:53:23 +0100

    Add sensors update in system task

--- components/system/system.c
+++ components/system/system.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 
 #include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 #include "system.h"
 #include "esp_log.h"
@@ -18,6 +19,12 @@ static bool is_init = false;
 void system_task(void *arg)
 {
     system_init();
+
+    while (1)
+    {
+        sensors_update_drone_data();
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
 }
 
 // This must be the first module to be initialized!
