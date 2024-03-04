76a74e0bc66686145751582781452bde8bd07836
jossebv <josebravopacheco@gmail.com>
2024-03-04 11:21:25 +0100

    Add system finite state machine structure

---
+++ components/system/system_fsm.c
@@ -0,0 +1,13 @@
+/**
+ * @file system_fsm.c
+ * @author Jose Manuel Bravo Pacheco
+ * @brief Logic for the system finite state machine
+ * @version 0.1
+ * @date 2024-03-04
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include "system.h"
+#include "fsm.h"
